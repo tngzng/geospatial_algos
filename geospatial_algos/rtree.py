da0959d433b8b8f490e3d66c92cbc66d76fe80ec
t zhang <tzcodes@fastmail.com>
2024-12-04 14:51:45 -0500

    [update] wip intersection w/ mock contains

--- geospatial_algos/rtree.py
+++ geospatial_algos/rtree.py
@@ -1,4 +1,4 @@
-'''
+"""
 The basic idea of an R-tree is simple: leaf nodes of the tree hold spatial data, 
 whereas a branching node corresponds to the minimum bounding box that contains all of its children.
 
@@ -24,7 +24,66 @@ list(idx.intersection(search_window))
 search_window = (1.01, 1.01, 2.0, 2.0) # ex. 4
 list(idx.intersection(search_window))
 >>> []
-'''
+
+Resources:
+- https://towardsdatascience.com/speed-up-your-geospatial-data-analysis-with-r-trees-4f75abdc6025
+- https://www.programiz.com/dsa/b-tree 
+- https://www.programiz.com/dsa/insertion-into-a-b-tree 
+"""
+
+from typing import Optional
+
+from shapely import box, contains
+
+Bounds = tuple[float, float, float, float]
+
+
+class Node:
+    def __init__(self, bounds: Bounds) -> None:
+        self.bbox = bounds
+        self.children = []
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name} {self.bbox}"
+
+
+def search(node: Node, bbox) -> Node:
+    print(f"searching node {node.bbox}")
+    """
+    return the node that is greater than or equal to the passed bbox
+    """
+
+
+def contains(x, y):
+    return x >= y
+
+
+one = Node(1)
+two = Node(2)
+three = Node(3)
+four = Node(4)
+five = Node(5)
+
+five.children = [two, four]
+two.children = [one]
+four.children = [three]
+
+found = search(five, 4)
+
 
 class Index:
-    pass
+    def __init__(self) -> None:
+        self.root: Node = None
+
+    def insert(self, label: str, bounds: Bounds) -> None:
+        if self.root is None:
+            self.root = Node(box(bounds))
+            return
+
+    def intersection(self, bounds: Bounds) -> None:
+        bbox = box(bounds)
+        # the outermost bounds of the dataset don't contain the query bounds
+        if not contains(self.root.bbox, bbox):
+            return []
+        parent_node = search(self.root, bbox)
