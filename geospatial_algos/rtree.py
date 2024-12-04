16f931e335ffefa4924087761f85f52d2f95ae8a
t zhang <tzcodes@fastmail.com>
2024-12-04 17:46:15 -0500

    [add] search implementation + test

--- geospatial_algos/rtree.py
+++ geospatial_algos/rtree.py
@@ -4,30 +4,10 @@ whereas a branching node corresponds to the minimum bounding box that contains a
 
 Functions:
 - insert - accept a label and a bounding box to add to the index
-- intersection - accept a bounding box to check for overlaps in the index 
-
-idx = Index()
-bbox_0 = (0.0, 0.0, 1.0, 1.0) # (left, bottom, right, top)
-bbox_1 = (3.0, 3.0, 6.0, 6.0)
-idx.insert(0, bbox_0)
-idx.insert(1, bbox_1)
-
-search_window = (-1.0, -1.0, 2.0, 2.0) # ex. 1
-list(idx.intersection(search_window))
->>> [0]
-search_window = (4.0, 4.0, 5.0, 5.0)   # ex. 2
-list(idx.intersection(search_window))
->>> [1]
-search_window = (0.0, 0.0, 6.0, 6.0)   # ex. 3
-list(idx.intersection(search_window))
->>> [0, 1]
-search_window = (1.01, 1.01, 2.0, 2.0) # ex. 4
-list(idx.intersection(search_window))
->>> []
+- search - accept a bounding box to check for intersections in the index 
 
 Resources:
 - https://towardsdatascience.com/speed-up-your-geospatial-data-analysis-with-r-trees-4f75abdc6025
-- https://www.programiz.com/dsa/b-tree 
 - https://www.programiz.com/dsa/insertion-into-a-b-tree 
 """
 
@@ -39,37 +19,16 @@ Bounds = tuple[float, float, float, float]
 
 
 class Node:
-    def __init__(self, bounds: Bounds) -> None:
-        self.bbox = bounds
+    def __init__(self, bounds: Bounds, label: Optional[str] = None) -> None:
+        self.bbox = box(*bounds)
+        self.label = label
         self.children = []
 
     def __repr__(self):
-        class_name = type(self).__name__
-        return f"{class_name} {self.bbox}"
-
-
-def search(node: Node, bbox) -> Node:
-    print(f"searching node {node.bbox}")
-    """
-    return the node that is greater than or equal to the passed bbox
-    """
-
-
-def contains(x, y):
-    return x >= y
-
-
-one = Node(1)
-two = Node(2)
-three = Node(3)
-four = Node(4)
-five = Node(5)
-
-five.children = [two, four]
-two.children = [one]
-four.children = [three]
-
-found = search(five, 4)
+        description = type(self).__name__
+        if self.label:
+            description = f"{description} '{self.label}'"
+        return f"{description}: {self.bbox}"
 
 
 class Index:
@@ -80,10 +39,23 @@ class Index:
         if self.root is None:
             self.root = Node(box(bounds))
             return
+        # check if there is an appropriate node to append to
+        # use the intersection func for this?
+        # if no such node exists, make a new root node
+        # by merging the existing root node with the new bounds
+        # and giving the new root two children
+        #   - the prev root,
+        #   - and the new bounds minus the prev root bounds
 
-    def intersection(self, bounds: Bounds) -> None:
-        bbox = box(bounds)
+    def search(self, bbox) -> None:
         # the outermost bounds of the dataset don't contain the query bounds
         if not contains(self.root.bbox, bbox):
             return []
-        parent_node = search(self.root, bbox)
+        parent = self.root
+        while True:
+            if eligible_child := next(
+                (child for child in parent.children if contains(child.bbox, bbox)), None
+            ):
+                parent = eligible_child
+            else:
+                return parent
--- tests/test_rtree.py
+++ tests/test_rtree.py
@@ -1,21 +1,91 @@
-from typing import Iterator
-
-import geojson
+import pytest
+from shapely import bounds, union
 
 from geospatial_algos.geospatial_algos import rtree  # type: ignore
 
-PUBLIC_COURTS_FILE = "assets/bk_named_courts.geojsonl"
 
+def test_search():
+    # setup test data
+    south_oxford = {
+        "type": "Feature",
+        "properties": {"name": "South Oxford"},
+        "geometry": {"type": "Point", "coordinates": [-73.972129, 40.6840711]},
+    }
+    fort_greene = {
+        "type": "Feature",
+        "properties": {"name": "Fort Greene"},
+        "geometry": {"type": "Point", "coordinates": [-73.978382, 40.6898247]},
+    }
+    jackie_robinson = {
+        "type": "Feature",
+        "properties": {"name": "Jackie Robinson"},
+        "geometry": {"type": "Point", "coordinates": [-73.9284951, 40.6806745]},
+    }
+    decatur = {
+        "type": "Feature",
+        "properties": {"name": "Decatur"},
+        "geometry": {"type": "Point", "coordinates": [-73.9355312, 40.6818194]},
+    }
 
-def _load_test_data(test_file: str) -> Iterator[dict]:
-    with open(test_file) as f:
-        for line in f:
-            datum = geojson.loads(line)
-            yield datum
+    def extract_bounds(geo_feature: dict) -> tuple:
+        x, y = geo_feature["geometry"]["coordinates"]
+        return (x, y, x, y)
 
+    def extract_name(geo_feature: dict) -> str:
+        return geo_feature["properties"]["name"]
 
-def test_insert():
+    south_oxford_node = rtree.Node(
+        extract_bounds(south_oxford),
+        extract_name(south_oxford),
+    )
+    fort_greene_node = rtree.Node(
+        extract_bounds(fort_greene),
+        extract_name(fort_greene),
+    )
+    jackie_robinson_node = rtree.Node(
+        extract_bounds(jackie_robinson), extract_name(jackie_robinson)
+    )
+    decatur_node = rtree.Node(extract_bounds(decatur), extract_name(decatur))
+
+    def get_parent_bounds(node1, node2):
+        parent_geom = union(node1.bbox, node2.bbox)
+        return bounds(parent_geom)
+
+    fort_greene_clinton_hill_node = rtree.Node(
+        get_parent_bounds(fort_greene_node, south_oxford_node),
+        "Fort Greene / Clinton Hill",
+    )
+    bedstuy_node = rtree.Node(
+        get_parent_bounds(jackie_robinson_node, decatur_node), "BedStuy"
+    )
+    root_node = rtree.Node(
+        get_parent_bounds(fort_greene_clinton_hill_node, bedstuy_node), "Root"
+    )
+    root_node.children = [bedstuy_node, fort_greene_clinton_hill_node]
+    bedstuy_node.children = [decatur_node, jackie_robinson_node]
+    fort_greene_clinton_hill_node = [fort_greene_node, south_oxford_node]
     idx = rtree.Index()
-    public_courts = _load_test_data(PUBLIC_COURTS_FILE)
-    for court in public_courts:
-        idx.insert(court["geometry"]["coordinates"])
+    idx.root = root_node
+
+    # test search
+    node = idx.search(decatur_node.bbox)
+    assert node == bedstuy_node
+
+
+@pytest.mark.skip
+def test_insert():
+    """
+    index the following point geometries:
+
+    A-B---
+    ------
+    ------
+    C-D--E
+    """
+    pass
+    # idx = rtree.Index()
+    # idx.insert("A", (1, 1, 1, 1))
+    # idx.insert("C", (4, 1, 4, 1))
+    # idx.insert("B", (1, 3, 1, 3))
+    # idx.insert("E", (4, 5, 4, 5))
+    # idx.insert("D", (4, 3, 4, 3))
