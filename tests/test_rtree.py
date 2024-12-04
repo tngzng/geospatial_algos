e187b3fbad441da691b71981266ef494796dbad1
t zhang <tzcodes@fastmail.com>
2024-12-04 14:51:11 -0500

    [update] add test file and load helper

--- tests/test_rtree.py
+++ tests/test_rtree.py
@@ -1,4 +1,21 @@
-from geospatial_algos.geospatial_algos import rtree
+from typing import Iterator
 
-def test_initialize_index():
-    rtree.Index()
+import geojson
+
+from geospatial_algos.geospatial_algos import rtree  # type: ignore
+
+PUBLIC_COURTS_FILE = "assets/bk_named_courts.geojsonl"
+
+
+def _load_test_data(test_file: str) -> Iterator[dict]:
+    with open(test_file) as f:
+        for line in f:
+            datum = geojson.loads(line)
+            yield datum
+
+
+def test_insert():
+    idx = rtree.Index()
+    public_courts = _load_test_data(PUBLIC_COURTS_FILE)
+    for court in public_courts:
+        idx.insert(court["geometry"]["coordinates"])
