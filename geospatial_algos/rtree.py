55477ac0500d0317c96f4898a2a2dd5967d6c7f0
t zhang <tzcodes@fastmail.com>
2024-12-05 12:06:26 -0500

    [add] find_parent_node, get_leaf_nodes, filter_intersecting

--- geospatial_algos/rtree.py
+++ geospatial_algos/rtree.py
@@ -1,19 +1,19 @@
 """
-The basic idea of an R-tree is simple: leaf nodes of the tree hold spatial data, 
+The basic idea of an R-tree is simple: leaf nodes of the tree hold spatial data,
 whereas a branching node corresponds to the minimum bounding box that contains all of its children.
 
 Functions:
 - insert - accept a label and a bounding box to add to the index
-- search - accept a bounding box to check for intersections in the index 
+- search - accept a bounding box to check for intersections in the index
 
 Resources:
 - https://towardsdatascience.com/speed-up-your-geospatial-data-analysis-with-r-trees-4f75abdc6025
-- https://www.programiz.com/dsa/insertion-into-a-b-tree 
+- https://www.programiz.com/dsa/insertion-into-a-b-tree
 """
 
 from typing import Optional
 
-from shapely import box, contains
+from shapely import Polygon, bounds, box, contains, difference, intersection, union
 
 Bounds = tuple[float, float, float, float]
 
@@ -30,27 +30,51 @@ class Node:
             description = f"{description} '{self.label}'"
         return f"{description}: {self.bbox}"
 
+    def add_child(self, child: "Node") -> None:
+        self.children.append(child)
+
 
 class Index:
     def __init__(self) -> None:
         self.root: Node = None
 
     def insert(self, label: str, bounds: Bounds) -> None:
-        if self.root is None:
-            self.root = Node(box(bounds))
-            return
-        # check if there is an appropriate node to append to
-        # use the intersection func for this?
-        # if no such node exists, make a new root node
-        # by merging the existing root node with the new bounds
-        # and giving the new root two children
-        #   - the prev root,
-        #   - and the new bounds minus the prev root bounds
+        child = Node(bounds, label=label)
 
-    def search(self, bbox) -> None:
-        # the outermost bounds of the dataset don't contain the query bounds
+        # scenario 1: index is empty
+        if self.root is None:
+            self.root = Node(bounds)
+            self.root.add_child(child)
+            return
+
+        # scenario 2: index has parent with bounds containing new child
+        bbox = box(*bounds)
+        parent = self.find_parent_node(bbox)
+        if parent:
+            parent.add_child(child)
+            return
+
+        # scenario 3: index needs new parent to contain new child
+        new_root_bounds = self.get_union_bounds(self.root.bbox, bbox)
+        new_root = Node(box(*new_root_bounds))
+
+        new_parent_bbox = difference(new_root.bbox, self.root.bbox)
+        new_parent = Node(bounds(new_parent_bbox), label=label)
+
+        new_parent.add_child(child)
+        new_root.add_child(new_parent)
+        new_root.add_child(self.root)
+        self.root = new_root
+
+    def get_union_bounds(bbox_1: Polygon, bbox_2: Polygon) -> Bounds:
+        union_geom = union(bbox_1, bbox_2)
+        return bounds(union_geom)
+
+    def find_parent_node(self, bbox: Polygon) -> Optional[Node]:
+        # the outermost bounds of the dataset don't contain the bounds
         if not contains(self.root.bbox, bbox):
-            return []
+            return None
+
         parent = self.root
         while True:
             if eligible_child := next(
@@ -59,3 +83,29 @@ class Index:
                 parent = eligible_child
             else:
                 return parent
+
+    def _get_leaf_nodes(self, parent: Node, leaf_nodes: list[Node]) -> None:
+        if children := parent.children:
+            for child in children:
+                self._get_leaf_nodes(child, leaf_nodes)
+        else:
+            leaf_nodes.append(parent)
+
+    def get_leaf_nodes(self, parent: Node) -> list[Node]:
+        leaf_nodes = []
+        self._get_leaf_nodes(parent, leaf_nodes)
+        return leaf_nodes
+
+    def filter_intersecting(self, nodes: list[Node], bbox: Polygon) -> list[Node]:
+        return [
+            node for node in nodes if node.bbox == bbox or intersection(node.bbox, bbox)
+        ]
+
+    def search(self, bounds: Bounds) -> list[Node]:
+        bbox = box(*bounds)
+        parent = self.find_parent_node(bbox)
+        if parent is None:
+            return []
+
+        leaf_nodes = self.get_leaf_nodes(parent)
+        return self.filter_intersecting(leaf_nodes, bbox)
--- tests/test_rtree.py
+++ tests/test_rtree.py
@@ -4,6 +4,83 @@ from shapely import bounds, union
 from geospatial_algos.geospatial_algos import rtree  # type: ignore
 
 
+def test_find_parent_node():
+    # setup test data
+    south_oxford = {
+        "type": "Feature",
+        "properties": {"name": "South Oxford"},
+        "geometry": {"type": "Point", "coordinates": [-73.972129, 40.6840711]},
+    }
+    fort_greene = {
+        "type": "Feature",
+        "properties": {"name": "Fort Greene"},
+        "geometry": {"type": "Point", "coordinates": [-73.978382, 40.6898247]},
+    }
+    jackie_robinson = {
+        "type": "Feature",
+        "properties": {"name": "Jackie Robinson"},
+        "geometry": {"type": "Point", "coordinates": [-73.9284951, 40.6806745]},
+    }
+    decatur = {
+        "type": "Feature",
+        "properties": {"name": "Decatur"},
+        "geometry": {"type": "Point", "coordinates": [-73.9355312, 40.6818194]},
+    }
+
+    def extract_bounds(geo_feature: dict) -> tuple:
+        x, y = geo_feature["geometry"]["coordinates"]
+        return (x, y, x, y)
+
+    def extract_name(geo_feature: dict) -> str:
+        return geo_feature["properties"]["name"]
+
+    south_oxford_node = rtree.Node(
+        extract_bounds(south_oxford),
+        extract_name(south_oxford),
+    )
+    fort_greene_node = rtree.Node(
+        extract_bounds(fort_greene),
+        extract_name(fort_greene),
+    )
+    jackie_robinson_node = rtree.Node(
+        extract_bounds(jackie_robinson), extract_name(jackie_robinson)
+    )
+    decatur_node = rtree.Node(extract_bounds(decatur), extract_name(decatur))
+
+    def get_parent_bounds(node1, node2):
+        parent_geom = union(node1.bbox, node2.bbox)
+        return bounds(parent_geom)
+
+    fort_greene_clinton_hill_node = rtree.Node(
+        get_parent_bounds(fort_greene_node, south_oxford_node),
+        "Fort Greene / Clinton Hill",
+    )
+    bedstuy_node = rtree.Node(
+        get_parent_bounds(jackie_robinson_node, decatur_node), "BedStuy"
+    )
+    root_node = rtree.Node(
+        get_parent_bounds(fort_greene_clinton_hill_node, bedstuy_node), "Root"
+    )
+    root_node.children = [bedstuy_node, fort_greene_clinton_hill_node]
+    bedstuy_node.children = [decatur_node, jackie_robinson_node]
+    fort_greene_clinton_hill_node.children = [fort_greene_node, south_oxford_node]
+    idx = rtree.Index()
+    idx.root = root_node
+
+    # test find_parent_node
+    node = idx.find_parent_node(decatur_node.bbox)
+    assert node == bedstuy_node
+
+    node = idx.find_parent_node(jackie_robinson_node.bbox)
+    assert node == bedstuy_node
+
+    node = idx.find_parent_node(south_oxford_node.bbox)
+    assert node == fort_greene_clinton_hill_node
+
+    node = idx.find_parent_node(fort_greene_node.bbox)
+    assert node == fort_greene_clinton_hill_node
+
+
 def test_search():
     # setup test data
     south_oxford = {
@@ -68,17 +145,9 @@ def test_search():
     idx.root = root_node
 
     # test search
-    node = idx.search(decatur_node.bbox)
-    assert node == bedstuy_node
-
-    node = idx.search(jackie_robinson_node.bbox)
-    assert node == bedstuy_node
-
-    node = idx.search(south_oxford_node.bbox)
-    assert node == fort_greene_clinton_hill_node
-
-    node = idx.search(fort_greene_node.bbox)
-    assert node == fort_greene_clinton_hill_node
+    nodes = idx.search(bounds(decatur_node.bbox))
+    assert len(nodes) == 1
+    assert nodes[0] == decatur_node
 
 
 @pytest.mark.skip
