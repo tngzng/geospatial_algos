1c86824aedf4b5f2d65aa8cd5a2dc7b03659ba8f
t zhang <tzcodes@fastmail.com>
2024-12-09 11:11:46 -0500

    [add] pseudocode; [add] test data

--- geospatial_algos/geo_utils.py
+++ geospatial_algos/geo_utils.py
@@ -4,6 +4,7 @@ this wrapper takes the convention that every function should be a verb,
 so appends "get_" or "make_" onto functions named as nouns
 """
 
+from shapely import LineString  # noqa
 from shapely import Polygon  # noqa
 from shapely import contains  # noqa
 from shapely import union  # noqa
--- geospatial_algos/line_simplification.py
+++ geospatial_algos/line_simplification.py
@@ -8,3 +8,26 @@ Resources:
 - https://cartography-playground.gitlab.io/playgrounds/douglas-peucker-algorithm/
 - https://medium.com/@indemfeld/the-ramer-douglas-peucker-algorithm-d542807093e7
 """
+
+from .geo_utils import LineString
+
+
+def simplify(polyline: LineString, epsilon: float = 0.0) -> LineString:
+    pass
+    # take first and last point in polyline
+    # define the baseline as a new line from those two points
+    # for all points in the polyline, identify the
+    # first point (notable point or significant point or good point)
+    # whose distance from the baseline exceeds epsilon
+    # keep that point and discard intermediary points
+    # (points between the first point in the polyline
+    # and the notable point that exceeded the baseline)
+    # if no notable point is found, the geometry has been simplified!
+    # if a notable point is found, make two new polylines
+    #   one starting with the notable point and
+    #   ending with the last point in the original polyline
+    #
+    #   one starting with the first point in the original polyline
+    #   and ending with the notable point
+
+    #   simplify the two new polylines
