f2076309e5e1bc145229312d93b0737fbfb318ac
t zhang <tzcodes@fastmail.com>
2024-12-11 18:21:46 -0500

    [add] check convex logic; [add] tests

--- geospatial_algos/convex_hull.py
+++ geospatial_algos/convex_hull.py
@@ -1,13 +1,29 @@
 """
 Resources:
 - https://medium.com/@errazkim/computing-the-convex-hull-in-python-60a6087e0faa
+- https://medium.com/@pascal.sommer.ch/a-gentle-introduction-to-the-convex-hull-problem-62dfcabee90c
 - https://www.mathsisfun.com/sine-cosine-tangent.html
 - https://www.mathsisfun.com/algebra/vectors-cross-product.html
+- https://www.geeksforgeeks.org/check-if-given-polygon-is-a-convex-polygon-or-not/
 """
 
 import numpy as np
 
 from .geo_utils import PointType, Polygon
+
+
+def make_vector(point_1: PointType, point_2: PointType) -> PointType:
+    return point_2[0] - point_1[0], point_2[1] - point_1[1]
+
+
+def check_convex(point_1: PointType, point_2: PointType, point_3: PointType) -> bool:
+    vector_1 = make_vector(point_1, point_2)
+    vector_2 = make_vector(point_1, point_3)
+    cross_product = np.cross(vector_1, vector_2)
+    # In cases where both input vectors have dimension 2,
+    # the z-component of the cross product is returned
+    # From: https://numpy.org/doc/stable/reference/generated/numpy.cross.html#numpy.cross
+    return bool(cross_product < 0)
 
 
 def convex_hull(points: list[PointType]) -> Polygon:
@@ -24,7 +40,7 @@ def convex_hull(points: list[PointType]) -> Polygon:
     anchor = min_point
     points = [p for p in points if p != anchor]
     # Sort the remaining points in order of increasing polar angle from the anchor point
-    # (if duplicates, order by increasing distance from P)
+    # (if duplicates, order by increasing distance from anchor point)
     points_by_angle: dict[float, PointType] = {}
     for point in points:
         x_dist = anchor[0] - point[0]
@@ -34,16 +50,20 @@ def convex_hull(points: list[PointType]) -> Polygon:
 
     sorted_angles = sorted(points_by_angle.keys())
 
-    # Initialize the hull with the anchor point P and the first element in the sorted list
+    # Initialize the hull with the anchor point and the first element in the sorted list
     hull: list[PointType] = [anchor, points_by_angle[sorted_angles[0]]]
 
     # Iterate over each point in the sorted list and determine if traversing to it from
     # the prior two points in the hull results in a concave or convex shape
     for angle in sorted_angles[1:]:
         point = points_by_angle[angle]
-        # TODO: If shape is concave with the preceeding two points, add it to the hull
-
-        # TODO: If shape is convex with the preceeding two points, remove the middle point
+        # If shape is convex with the preceeding two points, add it to the hull
+        if check_convex(hull[-2], hull[-1], point):
+            hull.append(point)
+        # If shape is concave with the preceeding two points, remove the middle point
         # and add the current point to the hull
+        else:
+            hull.pop()
+            hull.append(point)
 
-    return hull
+    return Polygon(hull)
--- tests/test_convex_hull.py
+++ tests/test_convex_hull.py
@@ -1,3 +1,4 @@
+import geojson
 import pytest
 
 from geospatial_algos.geospatial_algos import convex_hull  # type: ignore
@@ -45,5 +46,36 @@ def star_house():
     }
 
 
+def test_check_convex__is_convex():
+    point_1, point_2, point_3 = (
+        [-73.983146993549, 40.72154241600467],
+        [-73.98326651540587, 40.72159097703323],
+        [-73.98315564213495, 40.72180031349515],
+    )
+    assert convex_hull.check_convex(point_1, point_2, point_3) is True
+
+
+def test_check_convex__is_concave():
+    point_1, point_2, point_3 = (
+        [-73.98315564213495, 40.72180031349515],
+        [-73.98314301846652, 40.72164349109397],
+        [-73.9831273267312, 40.72170567587946],
+    )
+    assert convex_hull.check_convex(point_1, point_2, point_3) is False
+
+
 def test_convex_hull(star_house):
-    convex_hull.convex_hull(star_house["geometry"]["coordinates"][0])
+    hull = convex_hull.convex_hull(star_house["geometry"]["coordinates"][0])
+    assert geojson.loads(geojson.dumps(hull)) == {
+        "coordinates": [
+            [
+                [-73.983147, 40.721542],
+                [-73.983267, 40.721591],
+                [-73.983156, 40.7218],
+                [-73.982991, 40.721823],
+                [-73.982954, 40.721715],
+                [-73.983147, 40.721542],
+            ]
+        ],
+        "type": "Polygon",
+    }
