2d15ea11c5de68ea7dff05969e8589b9431e2ca1
t zhang <tzcodes@fastmail.com>
2024-12-12 17:47:46 -0500

    [add] benchmarking script

---
+++ geospatial_algos/scripts/benchmark_rtrees.py
@@ -0,0 +1,137 @@
+import random
+import sys
+import time
+
+sys.path.insert(0, ".")
+
+from geospatial_algos.geo_utils import get_intersection, make_bounds, make_box
+from geospatial_algos.rtree import Index
+
+public_courts: list[dict] = [
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "South Oxford",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.972129, 40.6840711]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Fort Greene",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.978382, 40.6898247]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Jackie Robinson",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.9284951, 40.6806745]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Decatur",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.9355312, 40.6818194]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Lincoln Terrace",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.925451, 40.668831]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Prospect Park",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.961844, 40.6549845]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Mccarren",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.9534005, 40.7222456]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Forest Hills",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.8904704, 40.6823958]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Leif Ericson",
+        },
+        "geometry": {"type": "Point", "coordinates": [-74.0171488, 40.6335201]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Cooper",
+        },
+        "geometry": {"type": "Point", "coordinates": [-73.9381716, 40.7162868]},
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Bensonhurst",
+        },
+        "geometry": {"type": "Point", "coordinates": [-74.0015034, 40.5950783]},
+    },
+]
+
+
+def extract_bounds(geo_feature: dict) -> tuple:
+    x, y = geo_feature["geometry"]["coordinates"]
+    return (x, y, x, y)
+
+
+def extract_name(geo_feature: dict) -> str:
+    return geo_feature["properties"]["name"]
+
+
+# populate index
+idx = Index()
+
+for court in public_courts:
+    idx.insert(extract_name(court), extract_bounds(court))
+
+# make it fair so we're not constantly making the bbox
+# in the brute force approach
+for court in public_courts:
+    court["bbox"] = make_box(extract_bounds(court))
+
+# test rtree
+# TODO: time this
+random.shuffle(public_courts)
+for court in public_courts:
+    found = idx.search(extract_bounds(court))
+    assert len(found) == 1
+    assert found[0].label == extract_name(court)
+
+
+# test brute force
+def brute_force_search(target: dict) -> list[dict]:
+    res = []
+    for court in public_courts:
+        if court["bbox"] == target["bbox"] or get_intersection(
+            court["bbox"], target["bbox"]
+        ):
+            res.append(court)
+
+    return res
+
+
+# TODO: time this
+random.shuffle(public_courts)
+for court in public_courts:
+    brute_found = brute_force_search(court)
+    assert len(brute_found) == 1
+    assert extract_name(brute_found[0]) == extract_name(court)
